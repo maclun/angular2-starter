a5ee5defd77c5e1ab68b49bd6d98eeebea60603f
Daniele Ghidoli <daniele.ghidoli@bigthink.it>
2016-06-24 19:23:20 +0200

    Added app providers

---
+++ src/app/app.providers.ts
@@ -0,0 +1,4 @@
+export const APP_PROVIDERS = [
+    // add common services accessible to every component
+    // put them in shared and add here
+];
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
-import { AppComponent, environment, APP_ROUTER_PROVIDERS } from './app/';
+import { AppComponent, environment, APP_ROUTER_PROVIDERS, APP_PROVIDERS } from './app/';
 
 if (environment.production) {
   enableProdMode();
 }
 
 bootstrap(AppComponent, [
-  APP_ROUTER_PROVIDERS
+  APP_ROUTER_PROVIDERS,
+  ...APP_PROVIDERS
 ]);
 
