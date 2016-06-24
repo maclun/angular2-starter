1e6a8cdea6c7b3d8d3d119482efc210d6e19620a
Daniele Ghidoli <daniele.ghidoli@bigthink.it>
2016-06-24 18:53:19 +0200

    Added support for scss files and variables + added main styles Thanks to https://www.codementor.io/angularjs/tutorial/compiling-sass-postcss-with-angular-cli

--- angular-cli-build.js
+++ angular-cli-build.js
@@ -1,13 +1,22 @@
-// Angular-CLI build configuration
-// This file lists all the node_modules files that will be used in a build
-// Also see https://github.com/angular/angular-cli/wiki/3rd-party-libs
-
 /* global require, module */
+"use strict";
 
-var Angular2App = require('angular-cli/lib/broccoli/angular2-app');
+const Angular2App = require('angular-cli/lib/broccoli/angular2-app');
+const compileSass = require('broccoli-sass');
+const mergeTrees = require('broccoli-merge-trees');
+const _ = require('lodash');
+const glob = require('glob');
+const compileCSS = require('broccoli-postcss');
+const cssnext = require('postcss-cssnext');
+const cssnano = require('cssnano');
 
 module.exports = function(defaults) {
-  return new Angular2App(defaults, {
+  let appTree = new Angular2App(defaults, {
+    sassCompiler: {
+      includePaths: [
+        'src/style'
+      ]
+    },
     vendorNpmFiles: [
       'systemjs/dist/system-polyfills.js',
       'systemjs/dist/system.src.js',
@@ -18,4 +27,27 @@ module.exports = function(defaults) {
       '@angular/**/*.+(js|js.map)'
     ]
   });
+  let sass = mergeTrees(_.map(glob.sync('src/**/*.scss'), function(sassFile) {
+      sassFile = sassFile.replace('src/', '');
+      return compileSass(['src'], sassFile, sassFile.replace(/.scss$/, '.css'));
+  }));
+  let css = compileCSS(sass, {
+    plugins: [
+      {
+        module: cssnext,
+        options: {
+            browsers: ['> 1%'],
+            warnForDuplicates: false
+        }
+      },
+      {
+        module: cssnano,
+        options: {
+            safe: true,
+            sourcemap: true
+        }
+      }
+    ]
+  });
+  return mergeTrees([appTree, sass, css], { overwrite: true });
 };
