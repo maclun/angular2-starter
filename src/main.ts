443f46a834cccd4e987f6bfe60c47f945aca3654
Daniele Ghidoli <daniele.ghidoli@bigthink.it>
2016-07-17 20:05:45 +0200

    Add HTTP_PROVIDERS to app bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { disableDeprecatedForms, provideForms } from '@angular/forms';
+import { HTTP_PROVIDERS } from '@angular/http';
 import { AppComponent, environment, APP_ROUTER_PROVIDERS, APP_PROVIDERS } from './app/';
 
 if (environment.production) {
@@ -10,6 +11,7 @@ if (environment.production) {
 bootstrap(AppComponent, [
   disableDeprecatedForms(),
   provideForms(),
+  HTTP_PROVIDERS,
   APP_ROUTER_PROVIDERS,
   ...APP_PROVIDERS
 ]);
